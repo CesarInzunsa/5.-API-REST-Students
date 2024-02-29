ba858ab395ed6e6016341f83f6cad1866348d6fa
Cesar Inzunsa <cealinzunsadi@ittepic.edu.mx>
2024-02-29 08:10:44 -0700

    ya funciona

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const studentRoutes = require('./routes/student.routes');
-const authmiddleware = require('./middleware/authMiddleware');
-const authutils = require('./middleware/authUtils');
 
 const app = express();
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username === 'admin' && password === 'admin') {
-        const token = authutils.generateToken({id: 1, username: username});
-        res.json({token});
-    } else {
-        res.json(401).json({error: "Unauthorized"});
-    }
-});
 
 app.use(bodyParser.json());
-app.use(authmiddleware);
 app.use('/students', studentRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutandose en el puerto ${PORT}`);
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const studentController = require('../controller/student.controller');
-const authUtils = require('../middleware/authUtils');
-
-function authenticate(req, res, next) {
-    const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(401).json({status: 401, message: 'Unauthorized'});
-    }
-
-    const decodedToken = authUtils.verifyToken(token);
-    if (!decodedToken) {
-        return res.status(401).json({status: 401, message: 'Unauthorized'});
-    }
-
-    req.user = decodedToken;
-    next();
-};
 
 // ----------------------- ROUTES ----------------------- //
 
-
 // Get all students
-router.get('/', authenticate, (req, res) => {
+router.get('/', (req, res) => {
     const students = studentController.getAllStudents();
     res.status(students.status).json(students);
 });
