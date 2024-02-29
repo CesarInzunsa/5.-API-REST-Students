1b03737b706a2798955d3a1b30595a1c8b5663d1
Cesar Inzunsa <cealinzunsadi@ittepic.edu.mx>
2024-02-29 07:41:54 -0700

    routes

---
+++ routes/student.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const studentController = require('../controller/student.controller');
+const authUtils = require('../middleware/authUtils');
+
+function authenticate(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({status: 401, message: 'Unauthorized'});
+    }
+
+    const decodedToken = authUtils.verifyToken(token);
+    if (!decodedToken) {
+        return res.status(401).json({status: 401, message: 'Unauthorized'});
+    }
+
+    req.user = decodedToken;
+    next();
+};
+
+// ----------------------- ROUTES ----------------------- //
+
+// Get all tasks
+router.get('/', authenticate, (req, res) => {
+    const students = studentController.getAllStudents();
+    res.status(students.status).json(students);
+});
